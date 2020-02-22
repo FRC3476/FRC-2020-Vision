300f3f829f71a5bcbe2f29485ffc9d8d5af6f849
Ashwin Gupta <ashwingupta2000@gmail.com>
2020-02-21 20:10:13 -0800

    added corner detector

--- vision.cpp
+++ vision.cpp
@@ -16,6 +16,8 @@
 #define HIGH_EXP 0.03
 #define LOW_EXP 0.001
 
+#define DEBUG
+
 using namespace cv;
 using namespace std;
 
@@ -56,6 +58,25 @@ int maxElem(vector<int> v) {
 float magnitude(Point2d p) {
 	return sqrt(p.x * p.x + p.y * p.y);
 
+}
+
+bool intersection(Point2f o1, Point2f p1, Point2f o2, Point2f p2, Point2f &r) {
+    Point2f x = o2 - o1;
+    Point2f d1 = p1 - o1;
+    Point2f d2 = p2 - o2;
+
+    float cross = d1.x*d2.y - d1.y*d2.x;
+    if (abs(cross) < /*EPS*/1e-8)
+        return false;
+
+    double t1 = (x.x * d2.y - x.y * d2.x)/cross;
+    r = o1 + d1 * t1;
+    return true;
+}
+
+//line intersection 
+bool findIntersection(Vec4f line, Vec4f line2 , Point2f &returnPoint ) {
+	return intersection(Point(line[2]+line[0]*1000, line[3]+line[1]*1000), Point(line[2], line[3]), Point(line2[2]+line2[0]*1000, line2[3]+line2[1]*1000), Point(line2[2], line2[3] ), returnPoint);
 }
 
 
@@ -116,6 +137,11 @@ void updateFPS(std::chrono::microseconds cur, std::chrono::microseconds prevTime
 		putText(drawing, fpsStr, Point(590, 10), FONT_HERSHEY_SIMPLEX, 0.5, COLOR_RED, 2, LINE_AA);
 }
 
+void drawLine(Mat m, Vec4f line) {
+	line(m, Point(leftLine[2]-leftLine[0] * 1000, leftLine[3]-leftLine[1] * 1000), 
+		Point(leftLine[2]+leftLine[0] * 1000, leftLine[3]+leftLine[1] * 1000), Scalar(128));
+}
+
 int main(int argc, char** argv ) {
 	
 	setupCam(stream);
@@ -128,6 +154,7 @@ int main(int argc, char** argv ) {
 	//main vision loop
 	while(1) { 
 		//keep track of times, request camera frame
+		if( (cv::waitKey(1) & 0xFF) == ' ');
 		auto cur = std::chrono::high_resolution_clock::now();
 		auto prevTime = cur;
 		if(!stream.isOpened()) return -1;
@@ -176,14 +203,88 @@ int main(int argc, char** argv ) {
 			minX = min(minX, contours[maxIndex][i].x);
 		}
 		
+		int searchStartY = (int) (minY+(maxY-minY)/3);
+		int searchEndY = (int) (maxY-(maxY-minY)/3);
+		int searchStartX = (int) (minX+(maxX-minX)/3);
+		int searchEndX = (int) (maxX-(maxX-minX)/3);
+		
+		#ifdef DEBUG 
+		line(contDisplay, Point(0, SearchStartY), Point(640, SearchStartY), Scalar(190), 4);
+		line(contDisplay, Point(0, SearchEndY), Point(640, SearchEndY), Scalar(190), 4);
+		#endif
+		
+		
+		vector<Point> leftPoints, rightPoints, bottomPoints, topPoints, innerLeftPoints, innerRightPoints, innerBottomPoints;
+		// find points for outside left, outside right, inner left, and inner right lines 
+		for(int i = searchStartY; i < searchEndY; i++) {	
+			leftPoints.push_back(Point(minElem( ytoxMap[i] ), i) );
+			rightPoints.push_back(Point(maxElem( ytoxMap[i] ), i) );
+			if(ytoxMap[i].size() == 4) {
+				std::sort(ytoxMap[i].begin(), ytoxMap[i].end());
+				//circle( contDisplay, Point(ytoxMap[i][2], i), 2, Scalar(64));
+				innerLeftPoints.push_back(Point(ytoxMap[i][1], i));
+				innerRightPoints.push_back(Point(ytoxMap[i][2], i));
+			}
+		}
+		// find points for bottom line and inner bottom line
+		for(int i = searchStartX; i < searchEndX; i++) {
+			bottomPoints.push_back(Point(i, maxElem( xtoyMap[i] )) );
+			if(i > searchStartX + (maxX-minX)/8 && i < searchEndX - (maxX-minX)/8) {
+				//circle( contDisplay, Point(i, minElem( xtoyMap[i] )), 2, Scalar(64));
+				innerBottomPoints.push_back(Point(i, minElem( xtoyMap[i] )));
+			}	
+		}
+		// find points for top line 
+		topPoints.push_back(Point(minX+2, minElem( xtoyMap[minX+2] )) );
+		topPoints.push_back(Point(maxX-2, minElem( xtoyMap[maxX-2] )) );
+		
+		// could not find enough points to make lines 
+		if(leftPoints.size() < 2 || rightPoints.size() < 2 || innerLeftPoints.size() < 2 
+			|| innerRightPoints.size() < 2 || innerBottomPoints.size() < 2 || topPoints.size() < 2
+			|| bottomPoints.size() < 2) continue;
+	
+		// fit lines to points via least squares reg 
+		Vec4f leftLine, rightLine, bottomLine, topLine, innerLeftLine, innerRightLine, innerBottomLine;
+		fitLine(leftPoints, leftLine, CV_DIST_L2, 0, 0.01, 0.01);
+		fitLine(rightPoints, rightLine, CV_DIST_L2, 0, 0.01, 0.01);
+		fitLine(bottomPoints, bottomLine, CV_DIST_L2, 0, 0.01, 0.01);
+		fitLine(topPoints, topLine, CV_DIST_L2, 0, 0.01, 0.01);
+		fitLine(innerLeftPoints, innerLeftLine, CV_DIST_L2, 0, 0.01, 0.01);
+		fitLine(innerRightPoints, innerRightLine, CV_DIST_L2, 0, 0.01, 0.01);	
+		fitLine(innerBottomPoints, innerBottomLine, CV_DIST_L2, 0, 0.01, 0.01);
+		
+		// find points of intersection for hexagon corners 
+		Point2f bottomLeft, bottomRight, topLeft, topRight, innerBottomLeft, innerBottomRight, innerTopLeft, innerTopRight;
+		bool a = findIntersection(leftLine, bottomLine , bottomLeft);
+		a = a&& findIntersection(rightLine, bottomLine , bottomRight);
+		a = a&& findIntersection(topLine, leftLine , topLeft);
+		a = a&& findIntersection(topLine, rightLine , topRight);
+		a = a&& findIntersection(innerBottomLine, innerLeftLine , innerBottomLeft);
+		a = a&& findIntersection(innerBottomLine, innerRightLine , innerBottomRight);
+		a = a&& findIntersection(topLine, innerLeftLine , innerTopLeft);
+		a = a&& findIntersection(topLine, innerRightLine , innerTopRight);
+		
+		drawLine(contDisplay, bottomLine);
+		drawLine(contDisplay, leftLine);
+		drawLine(contDisplay, rightLine);
+		drawLine(contDisplay, topLine);
 
-
+		if(!a) continue;
 		
+		// append points hexagon corners to array 
+		std::vector<cv::Point2f> corners; 
+		corners.push_back(bottomLeft);
+		corners.push_back(bottomRight);
+		corners.push_back(topLeft);
+		corners.push_back(topRight); 
+		corners.push_back(innerTopLeft);
+		corners.push_back(innerTopRight);	
+		corners.push_back(innerBottomLeft);	
+		corners.push_back(innerBottomRight);				
+		for(int i = 0; i < corners.size(); i++ ){
+			circle( frame, corners[i], 0, Scalar(255, 0, 0));			
+		}
 		
-		
-		
-		
-
 	
 		updateFPS();
 		sendUDP(data);
